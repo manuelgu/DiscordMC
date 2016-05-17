fa9d7f8b08e5d7d0a74f1013db89f6ee31147410
manuelgu <code@manuelgu.eu>
2016-05-17 15:41:44 +0200

    Minor

--- src/main/java/eu/manuelgu/discordmc/listener/DiscordEventListener.java
+++ src/main/java/eu/manuelgu/discordmc/listener/DiscordEventListener.java
@@ -121,7 +121,6 @@ public class DiscordEventListener {
 
     @EventSubscriber
     public void onReady(final ReadyEvent event) {
-        plugin.getLogger().info("Successfully logged in with '" + event.getClient().getOurUser().getName() + "'");
         // Check for file encoder (emoji-related)
         switch (System.getProperty("file.encoding")) {
             case "UTF8":
