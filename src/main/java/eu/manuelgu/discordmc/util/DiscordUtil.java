ffb8a7a1e3e719b940cb8d22f51fa9095e824b47
manuelgu <code@manuelgu.eu>
2016-05-25 21:03:07 +0200

    Method whether channel is valid
    Valid means that he is on at least one of the channel list in the
    configuration file

--- src/main/java/eu/manuelgu/discordmc/util/DiscordUtil.java
+++ src/main/java/eu/manuelgu/discordmc/util/DiscordUtil.java
@@ -8,7 +8,11 @@ import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.util.DiscordException;
 import sx.blah.discord.util.HTTP429Exception;
 
+/**
+ * Util class for utility methods
+ */
 public class DiscordUtil {
+    public static List<String> names;
 
     /**
      * Logout the client
@@ -36,5 +40,19 @@ public class DiscordUtil {
         } catch (DiscordException ignored) {
             return false;
         }
+    }
+
+    /**
+     * Check whether a channel is valid to receive or send messages
+     *
+     * @param channel The channel to test
+     * @return True when channel is valid, False if otherwise
+     */
+    public static boolean isValidChannel(IChannel channel) {
+        return names.contains(channel.getName());
+    }
+
+    static {
+        names = DiscordMC.discordToMinecraft.stream().map(IChannel::getName).collect(Collectors.toList());
     }
 }
